f54756c472e1a9ad2f33d1424e7e49ea5fb6f4dd
David <45941940+Tubohaler@users.noreply.github.com>
2022-08-15 13:21:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   // Wishlist: Notifikation
 
   useEffect(() => {
-    socket.current = io("https://waiting2.herokuapp.com/");
+    socket.current = io(process.env.REACT_APP_waiting2);
 
     socket.current.on("connect", () => {
       console.log("Connected");
